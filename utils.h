f8c1b314e55501ce02f38d2cc8979b8703397640
diht404 <diht404@gmail.com>
2022-10-09 01:42:20 +0300

    pop [] added change version

--- utils.h
+++ utils.h
@@ -35,7 +35,7 @@ enum UTILS_ERRORS
 };
 
 const size_t COMPILATION_CONST = 0xDEADF00D;
-const size_t VERSION_CONST     = 4;
+const size_t VERSION_CONST     = 5;
 const char REGS_NAMES[5][4] = {"r0x", "rax", "rbx", "rcx", "rdx"};
 const uint8_t BUFFER_SIZE = 128;
 
