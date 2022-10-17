b442c3a2d62bb8a5ea9fec7aa7a9f5452e3cd599
diht404 <diht404@gmail.com>
2022-10-17 13:22:11 +0300

    refactoring

--- common/consts.h
+++ common/consts.h
@@ -10,11 +10,9 @@ const uint8_t IMM_MASK = 0x20;
 const int EPS = 8;
 const float SQUEEZE = 1.5;
 
-
-const size_t  COMPILATION_CONST = 0xDEADF00D;
-const size_t  VERSION_CONST     = 10;
-const char    REGS_NAMES[5][4]  = {"r0x", "rax", "rbx", "rcx", "rdx"};
-const uint8_t BUFFER_SIZE       = 128;
-
+const size_t COMPILATION_CONST = 0xDEADF00D;
+const size_t VERSION_CONST = 10;
+const char REGS_NAMES[5][4] = {"r0x", "rax", "rbx", "rcx", "rdx"};
+const uint8_t BUFFER_SIZE = 128;
 
 #endif //STACK_COMMON_CONSTS_H
