f05210602d87c9c86a8704f5c63e23fbc96ea7de
diht404 <diht404@gmail.com>
2022-10-20 20:27:46 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #define flags ""
 //#define asm_file "squares_pic"
 //#define asm_file "print_squares"
-#define asm_file "quadratic"
-//#define asm_file "factorial"
+//#define asm_file "quadratic"
+#define asm_file "factorial"
 //#define asm_file "data"
 
 int main()
